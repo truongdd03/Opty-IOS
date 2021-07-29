c83fe68cd015548cee9802e05819f724d4e5378a
Dong Truong <dong23102003@gmail.com>
2021-07-29 22:33:40 +0700

    Add table view

---
+++ Opty-IOS/ViewControllers/Homepage/DetailsTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsTableViewCell.swift
+//  Opty-IOS
+//
+//  Created by Dong Truong on 7/29/21.
+//
+
+import UIKit
+
+class DetailsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var CellLabel: UILabel!
+    
+    var cellLabel = "" {
+        didSet {
+            CellLabel.text = cellLabel
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Opty-IOS/ViewControllers/Homepage/HomepageViewController.swift
+++ Opty-IOS/ViewControllers/Homepage/HomepageViewController.swift
@@ -7,24 +7,31 @@
 
 import UIKit
 
-class HomepageViewController: UIViewController {
+class HomepageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Home"
+        tableView.delegate = self
+        tableView.dataSource = self
+        //let customCell = UINib(nibName: "DetailsCell", bundle: nil)
+        //tableView.register(customCell, forCellReuseIdentifier: "DetailsCell")
+
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailsCell") as! DetailsTableViewCell
+        cell.cellLabel = "This is row \(indexPath.row)"
+
+        return cell
+    }
 }
