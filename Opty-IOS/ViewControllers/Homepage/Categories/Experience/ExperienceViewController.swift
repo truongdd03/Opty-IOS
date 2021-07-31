9fd8419de3a94b6b02f8f679c11dff8919fd4e85
Dong Truong <dong23102003@gmail.com>
2021-07-31 16:03:21 +0700

    Update experience view

---
+++ Opty-IOS/ViewControllers/Homepage/Categories/Experience/ExperienceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExperienceViewController.swift
+//  Opty-IOS
+//
+//  Created by Dong Truong on 7/31/21.
+//
+
+import UIKit
+
+class ExperienceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Experience"
+    }
+
+    // MARK: Table view
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
