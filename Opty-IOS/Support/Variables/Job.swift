b189cb15584ab9ef2230a9454c6c36c48fa32acd
Dong Truong <dong23102003@gmail.com>
2021-07-31 19:10:25 +0700

    Add new class

--- Opty-IOS/Support/Variables/Job.swift
+++ Opty-IOS/Support/Variables/Job.swift
@@ -8,5 +8,13 @@
 import UIKit
 
 class Job: NSObject {
+    var company = ""
+    var role = ""
+    var tags: [String] = []
     
+    init(company: String, role: String, tags: [String]) {
+        self.company = company
+        self.role = role
+        self.tags = tags
+    }
 }
