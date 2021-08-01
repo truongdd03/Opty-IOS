fd916112bff95e4ed4c5fb579a3e24e42e185bfb
Dong Truong <dong23102003@gmail.com>
2021-08-01 14:15:35 +0700

    Show tags

--- Opty-IOS/ViewControllers/Homepage/Categories/Experience/AddExperienceViewController.swift
+++ Opty-IOS/ViewControllers/Homepage/Categories/Experience/AddExperienceViewController.swift
@@ -7,15 +7,19 @@
 
 import UIKit
 
-class AddExperienceViewController: PopUpViewController {
-
+class AddExperienceViewController: PopUpViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     @IBOutlet weak var CompanyInput: UITextField!
     @IBOutlet weak var RoleInput: UITextField!
     @IBOutlet weak var DuarationInput: UITextField!
     @IBOutlet weak var CancelButton: UIButton!
     @IBOutlet weak var AddButton: UIButton!
     @IBOutlet weak var JobDescriptionInput: UITextView!
+    
     @IBOutlet weak var TagsCollectionView: UICollectionView!
+    @IBOutlet weak var DeselectedTagsCollectionView: UICollectionView!
+    
+    var selectedTags: [String] = ["Swift", "Javascript", "C++", "CSS", "HTML", "Pascal"]
+    var deselectedTags: [String] = ["Python", "Ruby", "Java", "Koltin", "C#"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +30,11 @@ class AddExperienceViewController: PopUpViewController {
         Utilities.styleFilledButton(AddButton)
         Utilities.styleHollowButton(CancelButton)
         Utilities.styleTextView(textView: JobDescriptionInput)
+        
+        TagsCollectionView.delegate = self
+        TagsCollectionView.dataSource = self
+        DeselectedTagsCollectionView.delegate = self
+        DeselectedTagsCollectionView.dataSource = self
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
@@ -36,4 +45,22 @@ class AddExperienceViewController: PopUpViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    // MARK: Collection view
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if (collectionView == TagsCollectionView) {
+            return selectedTags.count
+        }
+        return deselectedTags.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if (collectionView == TagsCollectionView) {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Tag", for: indexPath) as! TagCollectionViewCell
+            cell.setLabel(tag: selectedTags[indexPath.item])
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DeselectedTag", for: indexPath) as! TagCollectionViewCell
+        cell.setLabel(tag: deselectedTags[indexPath.item])
+        return cell
+    }
 }
