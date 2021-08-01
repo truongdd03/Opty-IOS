6cad7c2051369bfa4204fef243137fed61282048
Dong Truong <dong23102003@gmail.com>
2021-08-01 13:42:24 +0700

    Design add-experience view

--- Opty-IOS/ViewControllers/Homepage/Categories/Experience/AddExperienceViewController.swift
+++ Opty-IOS/ViewControllers/Homepage/Categories/Experience/AddExperienceViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class AddExperienceViewController: UIViewController {
+class AddExperienceViewController: PopUpViewController {
 
+    @IBOutlet weak var CompanyInput: UITextField!
+    @IBOutlet weak var RoleInput: UITextField!
+    @IBOutlet weak var DuarationInput: UITextField!
+    @IBOutlet weak var CancelButton: UIButton!
+    @IBOutlet weak var AddButton: UIButton!
+    @IBOutlet weak var JobDescriptionInput: UITextView!
+    @IBOutlet weak var TagsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Utilities.styleSimpleTextField(textField: CompanyInput)
+        Utilities.styleSimpleTextField(textField: RoleInput)
+        Utilities.styleSimpleTextField(textField: DuarationInput)
+        Utilities.styleFilledButton(AddButton)
+        Utilities.styleHollowButton(CancelButton)
+        Utilities.styleTextView(textView: JobDescriptionInput)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
