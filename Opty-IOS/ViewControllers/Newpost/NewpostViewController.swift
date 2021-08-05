c9ac61c1da0f59caf31ac7bbec3bc5f032e5a776
Dong Truong <dong23102003@gmail.com>
2021-08-05 09:38:48 +0700

    create new post page

---
+++ Opty-IOS/ViewControllers/Newpost/NewpostViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewpostViewController.swift
+//  Opty-IOS
+//
+//  Created by Dong Truong on 8/5/21.
+//
+
+import UIKit
+
+class NewpostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "New post"
+    }
+
+}
